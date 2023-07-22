d97570c617932758ef169588e36cca27ac66962c
VitorHirt <zorinos.masterzorin@outlook.com>
2023-07-22 20:12:04 -0300

    StudyCs

---
+++ Estudos01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program{
+    internal class Program{
+        static void Main(string[] args){
+
+            string name = "Vitor";
+            char i = 'a';
+            int num = 1000;
+            long  num2 = 10203;
+            float num3 = 10.52f;
+            double num4 = 12.2012020;
+
+            Console.WriteLine( name + " - " + i + " - " + num + " - " + num2 + " - " + num3 + " - " + num4);
+        }
+    }
+}
