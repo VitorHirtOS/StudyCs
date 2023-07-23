1de5c573d8af3a32a4a27a9b56a5112f99ec804d
VitorHirt <zorinos.masterzorin@outlook.com>
2023-07-23 11:52:53 -0300

    study

--- Estudos01/Program.cs
+++ Estudos01/Program.cs
@@ -1,17 +1,44 @@
 ﻿using System;
+using Result;
 
-namespace Program{
-    internal class Program{
-        static void Main(string[] args){
-
-            string name = "Vitor";
+namespace SampleNamespace
+{
+    class Program
+    {
+        public static void Main()
+        {
             char i = 'a';
             int num = 1000;
-            long  num2 = 10203;
+            long num2 = 10203;
             float num3 = 10.52f;
             double num4 = 12.2012020;
 
-            Console.WriteLine( name + " - " + i + " - " + num + " - " + num2 + " - " + num3 + " - " + num4);
+            Console.WriteLine(i + " - " + num + " - " + num2 + " - " + num3 + " - " + num4);
+
+            Console.WriteLine(Retorno.RetornoSomar(Soma.Somar(num)));
+
+            SampleClass sampleObject = new SampleClass();
+            sampleObject.SampleMethod();
+
+            string nameValue = sampleObject.GetName();
+            Console.WriteLine("Valor de name: " + nameValue);
         }
     }
+
+
+    class Soma{
+        public static int Somar(int num){
+            return  num;
+        }
+    }
+
+    class Retorno
+    {
+        public static int RetornoSomar(int num)
+        {
+            int num1 = 1000;
+            return Soma.Somar(num + num1);
+        }
+    }
+
 }
---
+++ Estudos01/Result.cs
@@ -0,0 +1,18 @@
+namespace Result
+{
+    class SampleClass
+    {
+        private string name = "José"; // Variável name é privada dentro da classe SampleClass
+
+        public void SampleMethod()
+        {
+            name = "Vitor";
+        }
+
+        // Método público para retornar o valor da variável name
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
